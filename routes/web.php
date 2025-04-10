1a291e28daba012714ffbd8299ff9c1f0eb10e9e
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-10 08:08:44 +0530

    optional routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// optional routing
+Route::get('/products/{id?}', function ($id=null) {
+    if(!$id){
+        return "All Products";
+    }
+    return "product id: $id";
+});
