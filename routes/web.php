9d7ff4bd7ad4cc9295e59ebed2626b3098b3ddb5
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-11 08:07:01 +0530

    intial commit

--- routes/web.php
+++ routes/web.php
@@ -1,86 +1,89 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 // optional routing
-Route::get('/products/{id?}', function ($id=null) {
-    if(!$id){
-        return "All Product";
-    }
-    return "product id: $id";
-});
+// Route::get('/products/{id?}', function ($id=null) {
+//     if(!$id){
+//         return "All Product";
+//     }
+//     return "product id: $id";
+// });
 
 // route validation -----------------------
 
 // example 1: only number
-Route::get('/user/{id}',function($id){
-    return "user id: $id";
-})->whereNumber('id');
+// Route::get('/user/{id}',function($id){
+//     return "user id: $id";
+// })->whereNumber('id');
 
 // example 2: only alphabets
-Route::get('/category/{category}',function($category){
-    return "category type: $category";
-})->whereAlpha('category');
+// Route::get('/category/{category}',function($category){
+//     return "category type: $category";
+// })->whereAlpha('category');
 
 // example 3: alphanumric value
-Route::get('/username/{username}',function($username){
-    return "username: $username";
-})->whereAlphaNumeric('username');
+// Route::get('/username/{username}',function($username){
+//     return "username: $username";
+// })->whereAlphaNumeric('username');
 
 // example 4: must match one of the array element
- Route::get('/lang/{lang}',function($lang){
-    return "language: $lang";
-})->whereIn('lang',['en','hi','ur']);
+//  Route::get('/lang/{lang}',function($lang){
+//     return "language: $lang";
+// })->whereIn('lang',['en','hi','ur']);
 
 // example 5: Regex - only lowercase letters
-Route::get('/pass/{pass}',function($pass){
-    return "language: $pass";
-})->where('pass','[a-z]+');
+// Route::get('/pass/{pass}',function($pass){
+//     return "language: $pass";
+// })->where('pass','[a-z]+');
 
 // example 6: Regex - for multipe values
-Route::get('/lang/{lang}/course/{coursId}',function($lang,$coursId){
-    return "course: $coursId and language: $lang";
-})->where(['lang'=>'[a-z]{2}','coursId'=>'\d{4}']);
+// Route::get('/lang/{lang}/course/{coursId}',function($lang,$coursId){
+//     return "course: $coursId and language: $lang";
+// })->where(['lang'=>'[a-z]{2}','coursId'=>'\d{4}']);
 
 // example 7: Regex - with special chars
-Route::get('/search/{query}',function($query){
-    return "search query:$query";
-})->where('query','.+');
+// Route::get('/search/{query}',function($query){
+//     return "search query:$query";
+// })->where('query','.+');
 
 //named routes ----------------------
 
-route::get('userProfile',function(){
-    return 'user';
-})->name('user');
+// route::get('userProfile',function(){
+//     return 'user';
+// })->name('user');
 
-route::get('user',function(){
-    // return redirect()->route('user');
-    return to_route('user');
-});
-route::get('current-user',function(){
-    // return redirect()->route('user');
-    return to_route('user');
-});
+// route::get('user',function(){
+//     // return redirect()->route('user');
+//     return to_route('user');
+// });
+// route::get('current-user',function(){
+//     // return redirect()->route('user');
+//     return to_route('user');
+// });
 
 //route group
-route::prefix('admin')->group(function(){
-    Route::get('/settings',function(){
-        return "admin settings";
-    });
-    Route::get('/profile',function(){
-        return "profile";
-    });
-});
+// route::prefix('admin')->group(function(){
+//     Route::get('/settings',function(){
+//         return "admin settings";
+//     });
+//     Route::get('/profile',function(){
+//         return "profile";
+//     });
+// });
 
 // fallback
-route::fallback(function(){
-    return 'fallback route';
-});
+// route::fallback(function(){
+//     return 'fallback route';
+// });
 
 // challenge: return some of route parameters
-route::get('/sum/{num1}/{num2}',function($num1,$num2){
-    return "sum: $num1 + $num2 is".$num1+$num2;
-})->whereNumber('num1','num2');
+// route::get('/sum/{num1}/{num2}',function($num1,$num2){
+//     return "sum: $num1 + $num2 is".$num1+$num2;
+// })->whereNumber('num1','num2');
+
+// ----------------------------------------------------------------------------
