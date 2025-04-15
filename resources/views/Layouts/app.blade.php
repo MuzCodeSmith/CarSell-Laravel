0cab53c9a3c2c312f22e84e3374127e54b6a3837
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-15 09:06:49 +0530

    more directives on footer links

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -2,10 +2,9 @@
   @section('childContent')
     @include('Layouts.partials.header')
       @yield('content')
-    <footer>
-      @section('footerLinks')
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      @show
-    </footer>
+    @hasSection('footerLinks')
+      <footer>
+        @yield('footerLinks')
+      </footer>
+    @endif
   @endsection
