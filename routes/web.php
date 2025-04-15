345d724f2d9406414fa9ff668e27b968a74a0c0d
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-15 08:32:13 +0530

    add signup,route,controller and view

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 
 Route::get('/',[HomeController::class,'index']);
+Route::get('/signup',[SignupController::class,'create']);
