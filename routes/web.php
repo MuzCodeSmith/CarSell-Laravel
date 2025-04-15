6697cd20e853981232a940f4d1b2076c8ac9e1e6
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-15 08:39:01 +0530

    add login,route,controller and view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/',[HomeController::class,'index']);
 Route::get('/signup',[SignupController::class,'create']);
+Route::get('/login',[LoginController::class,'create']);
