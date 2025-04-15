34ca397f3ecae52c83578321fedf9ca4e717e8d2
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-15 08:57:28 +0530

    add footer links

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -2,5 +2,10 @@
   @section('childContent')
     @include('Layouts.partials.header')
       @yield('content')
-    <footer></footer>
+    <footer>
+      @section('footerLinks')
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      @show
+    </footer>
   @endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1007,4 +1007,11 @@
       </section>
       <!--/ New Cars -->
     </main>
+
+    @section('footerLinks')
+      @parent
+      <a href="#">Link 3</a>
+      <a href="#">Link 4</a>
+    @endsection
+
 @endsection
