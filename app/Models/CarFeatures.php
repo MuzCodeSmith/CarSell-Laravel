6e48dd23c813db6c35a886cc4b13a3ab11baf6f7
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-18 23:09:20 +0530

    did timestamps disable every model except car model

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CarFeatures extends Model
 {
-    //
+    public $timestamps = false;
+
 }
--- app/Models/CarImages.php
+++ app/Models/CarImages.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class CarImages extends Model
 {
-    //
+    public $timestamps = false;
+
 }
