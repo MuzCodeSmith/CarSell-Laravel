ab3aa8e055f7c949a1166fdc78d6723d1ae7d6fa
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-19 22:36:26 +0530

    inserted data into database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,12 +17,43 @@ class HomeController extends Controller
         
         // $cars = Car::orderBy('published_at','desc')->get();
 
-        $cars = Car::where('published_at','!=',null)
-        ->where('car_type_id',2)
-        ->orderBy('published_at')
-        ->limit(1)
-        ->get();
-        dump($cars);
+        // $cars = Car::where('published_at','!=',null)
+        // ->where('car_type_id',2)
+        // ->orderBy('published_at')
+        // ->limit(1)
+        // ->get();
+        // dump($cars);
+
+      
+
+        // approach 1
+        $cardata = [
+            'maker_id' => 1,
+            'model_id' => 1,
+            'price' => 35000,
+            'year' => 2030,
+            'vin' => 6,
+            'mileage' => 2,
+            'car_type_id' => 2,
+            'fuel_type_id' => 2,
+            'city_id' => 2,
+            'user_id' => 1,
+            'phone' => 9175826955,
+            'address' => 'dummy adderss',
+            'description' => 'dummy description',
+            'published_at' => now(),
+        ];
+        // $record = Car::create($cardata);
+
+    // Approach 2
+    $car2 = new Car();
+    $car2->fill($cardata);
+    $car2->save();
+
+    // Approch 3
+    // $car3= new Car($cardata);
+    // $car3->save();
+
 
         return view('home.index');
     }
--- app/Models/Car.php
+++ app/Models/Car.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Car extends Model
 {
     use SoftDeletes;
+
+    protected $fillable = [
+        'maker_id',
+        'model_id',
+        'year',
+        'price',
+        'vin',
+        'mileage',
+        'fuel_type_id',
+        'car_type_id',
+        'city_id',
+        'user_id',
+        'phone',
+        'address',
+        'description',
+        'published_at',
+    ];
+    // protected $guarded = [
+    //     'user_id',
+    // ];
 }
