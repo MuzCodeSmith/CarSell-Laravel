5d3eefe2153a2491e904808975a2e91eba70cff9
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-19 08:02:36 +0530

    practiced basic of orm

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        // $cars = Car::get();
+        
+        // $cars = Car::where('published_at','!=',null)->get();
+        
+        // $cars = Car::where('published_at','!=',null)->first();
+        
+        // $cars = Car::orderBy('published_at','desc')->get();
+
+        $cars = Car::where('published_at','!=',null)
+        ->where('car_type_id',2)
+        ->orderBy('published_at')
+        ->limit(1)
+        ->get();
+        dump($cars);
+
         return view('home.index');
     }
 }
