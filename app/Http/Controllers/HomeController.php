a02a780c8c3982ec1d8c5bccc1e370711cbfe2b5
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-19 23:26:10 +0530

    updating data of  database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,50 +9,19 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        // $cars = Car::get();
-        
-        // $cars = Car::where('published_at','!=',null)->get();
-        
-        // $cars = Car::where('published_at','!=',null)->first();
-        
-        // $cars = Car::orderBy('published_at','desc')->get();
 
-        // $cars = Car::where('published_at','!=',null)
-        // ->where('car_type_id',2)
-        // ->orderBy('published_at')
-        // ->limit(1)
-        // ->get();
-        // dump($cars);
+        // $car = Car::find(1);
+        // $car->price = 12000;
+        // $car->save(); 
 
-      
+        // Create or update
+        // $car = Car::updateOrCreate(
+        //     ['vin'=>666,'price'=>35000],
+        //     ['vin'=>999],
+        // );
 
-        // approach 1
-        $cardata = [
-            'maker_id' => 1,
-            'model_id' => 1,
-            'price' => 35000,
-            'year' => 2030,
-            'vin' => 6,
-            'mileage' => 2,
-            'car_type_id' => 2,
-            'fuel_type_id' => 2,
-            'city_id' => 2,
-            'user_id' => 1,
-            'phone' => 9175826955,
-            'address' => 'dummy adderss',
-            'description' => 'dummy description',
-            'published_at' => now(),
-        ];
-        // $record = Car::create($cardata);
-
-    // Approach 2
-    // $car2 = new Car();
-    // $car2->fill($cardata);
-    // $car2->save();
-
-    // Approch 3
-    $car3= new Car($cardata);
-    $car3->save();
+        // mass update
+            $car = Car::where('vin',6)->where('price',35000)->update(['vin'=>222]);
 
 
         return view('home.index');
